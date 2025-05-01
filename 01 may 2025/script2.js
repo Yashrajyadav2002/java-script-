39a2dc804a1f015b3e9545ca4895f80d96af7f9e
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-01 23:20:43 +0530

    01datatypes

---
+++ 01 may 2025/script2.js
@@ -0,0 +1,19 @@
+var name="yash";
+var age="21";
+
+console.log(name);
+console.log(age);
+
+//these are premative data type 
+
+var name="yash";
+console.log(name,typeof name);
+
+var num=36.6;
+console.log(num,typeof num);
+
+var num = 125565n;
+console.log(num, typeof num);
+
+var id= Symbol(id);
+console.log(id, typeof id);
