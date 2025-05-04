5f649f0dc3b51904e09701723b5044c9b4deefe0
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-04 13:12:56 +0530

    04maystring

---
+++ 04 may 2025/script5.js
@@ -0,0 +1,15 @@
+// single coat string ?
+
+let single = " HELLO BROTHER ";
+console.log(single);
+
+//double coat string?
+
+let double = 'who are you';
+console.log(double);
+
+//backticks (template literals)?
+
+let name =`my name is `;
+let backtik = `Hello , ${name}! yashraj `;
+console.log(backtik);
