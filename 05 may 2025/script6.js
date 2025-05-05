707c7b0408e4d70200250e27295135d4ddaf0607
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-05 18:08:09 +0530

    math and date objects

---
+++ 05 may 2025/script6.js
@@ -0,0 +1,69 @@
+//    MATH TYPE OBJECTS 
+
+// floor - returns the lower value
+
+let a= Math.floor(5.6780);
+console.log(a);
+
+//ceil - returns the upper value 
+
+let b = Math.ceil(3.6544);
+console.log(b);
+
+//trunc - returns the integer value
+
+let c = Math.trunc(2546.365);
+console.log(c);
+
+//MIN - returns the minium value
+
+let e = Math.min(35,19,29,59,99,89,78);
+console.log(e);
+
+//MAX - returns the maximum value 
+
+let f = Math.max(56,58,77,99,35,36);
+console.log(f);
+
+//POW - returns the power value
+
+let g = Math.pow(3,3);
+console.log(g);
+
+// RANDOM = returns the random value
+
+let h = Math.random()+1;
+console.log(h);
+
+
+
+
+let x = Math.random() * 51 +1;
+let y = Math.random() * 101 + 1;
+
+ let sum = x+y;
+ console.log(sum);
+
+ let div = x/y;
+ console.log(div);
+ 
+ let multi = x*y;
+ console.log(multi);
+
+ let min = x-y;
+ console.log(min);
+
+//      DATE TYPE OBJECTS 
+
+// by defult javascript will use the browser time line and display the date in string
+
+let d = new Date();
+
+console.log(d);
+
+// there are four ways to create object in date object
+
+let D =new Date(2025,4,3,25,36,10);
+console.log(D);
+
+
