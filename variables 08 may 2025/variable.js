d9a047959d2c91dc0e6a2506df1196d589a9b9e4
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-08 22:28:58 +0530

    variable

---
+++ variables 08 may 2025/variable.js
@@ -0,0 +1,21 @@
+// console is print the output
+console.log("Yashraj");
+
+let name ; //Declaration
+ name = "Yashraj Yadav"; //iniclazition
+console.log(name); // print the value by the console
+
+let village = "Bhopal";
+console.log(village);
+
+let a = 10,
+b = 20,
+c = 30,
+d = 40 ;
+console.log(a,b,c,d);
+
+// const value is not change in feature
+
+const pi = 20.1;
+console.log(pi);
+
