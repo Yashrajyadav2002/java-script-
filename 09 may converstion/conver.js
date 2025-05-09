f7134bab8977093faae7acce7119f7278a855159
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-09 18:36:12 +0530

    convertion

---
+++ 09 may converstion/conver.js
@@ -0,0 +1,25 @@
+// types of convertion
+var a =10;
+let y =a.toString(a);
+console.log(y,typeof y);
+
+// var b ="hello";
+// z = b.number(b);
+// console.log(z , typeof z);
+
+h = 50+"55";
+console.log(h);
+
+r =45.69;
+s = parseFloat(r);
+console.log(s);
+
+// //user input
+// let name =prompt("what is your name");
+// let age =parseInt(prompt("enter your age"));
+// console.log(name+age);
+
+
+let rec =prompt("enter your length");
+let angle =parseInt(prompt("enter the width"));
+console.log("area is" ,rec*angle);
--- 30 APRIL/script.js
+++ 30 APRIL/script.js
@@ -1,8 +1,44 @@
-alert("yash here")
-console.log("how are you buddy");
-console.log("whats going on");
+// alert("yash here")
+// // console.log("how are you buddy");
+// console.log("whats going on");
 
-console.error("this is error");
+// console.error("this is error");
 
-console.warn("warrning");
+// console.warn("warrning");
+
+
+// //math methods
+
+// x = str.ceil(4.1);
+// console.log();
+// x = str.min(4.1);
+// x = str.max(4.1);
+// x = str.sqrt(4.1);
+// x = str.pow(4.1);
+// x = str.floor(4.1);
+// x = str.trunc(4.1);
+// x = str.random(4.1);
+// x = str.round(4.1);
+
+// x = Math.random(2.1);
+// console.log(floor(random))
+
+
+
+// console.log(x);
+
+const d =new Date();
+console.log(d);
+D = new Date(2020,2);
+console.log(D);
+
+//miliseconds
+f = new Date(21611556);
+console.log(f);
+//string
+H =new Date("2023,3,15");
+console.log(typeof H);
+
+//type conversion
+
 
