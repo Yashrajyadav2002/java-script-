1f7e7b53deec251e154f64da6a2a274622839ebd
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-11 12:55:42 +0530

    datatype

---
+++ 11 may datatype/datatype script.js
@@ -0,0 +1,64 @@
+
+
+//number type= this is for all number and (integers and floats)
+
+var num=20;
+console.log(num,typeof num);
+
+let val=2807.7;
+console.log(val, typeof val);
+
+
+// string type (only text value)?
+var name="sonu";
+console.log(name, typeof name);
+
+let same="game";
+console.log(same, typeof same);
+
+const game="ram";
+console.log(game,typeof game);
+
+// boolen type ( for true and false)?
+
+let active=true;
+console.log(active,typeof active);
+
+var pre=false;
+console.log(pre,typeof pre);
+ 
+const post=true;
+console.log(post,typeof post);
+
+// bigint type it define the big number value?
+
+let big=156441656n;
+console.log(big,typeof big);
+
+var very=515665655n;
+console.log(very,typeof very);
+
+const little=564658465n;
+console.log(little,typeof little);
+
+// symbol type (ES6 it comes in 2015 )?
+
+let sym= ('@');
+    console.log(sym,typeof sym);
+
+//undifinded (a variable declered but not assigned a value)?
+
+let a;
+console.log(a, typeof a);
+ 
+var b;
+console.log(b,typeof b);
+
+//null is show the object value ?
+var value=null;
+console.log(value,typeof value);
+
+
+let person = {name:"yash",age:21};
+console.log(person.name);
+console.log(person.age);
