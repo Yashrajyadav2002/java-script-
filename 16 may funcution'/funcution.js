523a1dfee6459ae1b486941e04410a00b8719653
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-16 13:30:29 +0530

    funcation

---
+++ 16 may funcution'/funcution.js
@@ -0,0 +1,40 @@
+// funcation is a set of instruction . we save the code and ree use the funcation that is called funcation .. its a block code.\
+
+// there are three things  in funcation. ( funcation is not run without ie)
+// 0.1 (funcation delcarition)
+//0.2 funcation defination
+// 0.3 funcation calling
+
+// in a funcation decleration (parameter)
+// in  a funcation calling ( argument) 
+
+function Demo (){
+    console.log('Hello yash')
+};
+Demo();
+
+// write a program to take input from user and chek number is odd or even using funcation
+
+function Hello (){
+    let num = prompt("enter the num");
+
+    if (num%2!==0){
+        console.log("number is odd");
+    }
+    else{
+            console.log("number is even");
+        }
+
+}
+Hello();
+
+// write a program to take from user print reverse table of thet of a number.
+function who (){
+    let val =prompt ("enter the number");
+
+    for (let i =10;i>0;i--){
+        console.log(val,"x",i,'=',val*i)
+    }
+
+}
+who();
