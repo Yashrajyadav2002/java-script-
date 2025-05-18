021baaad726fb24f87cf6e1eeda29733744e7509
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-18 13:50:53 +0530

    stringquestions

--- 03 may 2025/script4.js
+++ 03 may 2025/script4.js
@@ -70,6 +70,52 @@ let mystring="Devloper";
 let mynewstring = mystring.chartAT(0) + mystring.substring();
 
 
+// now we are solve the string questions
+
+//Q.1 create a program to take full name from user and genurate a user name strart with followed by their full name end with under score followed by the length of full name
+
+let user = prompt("enter your name");
+let username = `@${user.trim()} _ ${user.length}`;
+console.log(username);
+
+// question no 2
+
+let val = prompt ("enter the string value");
+let ch = prompt ("enter the charactor");
+let count =1;
+for (char of val);
+if(char===ch){
+count++;
+}
+console.log(count);
+
+// question number 2 (face =2)
+let val7 = prompt ("enter the string value");
+let ch7 = prompt ("enter the charactor");
+let count7 =1;
+for (char of val);
+if(char.toLowerCase===ch7.toLowerCase){
+count++;
+}
+console.log(count);
+
+
+// let val = prompt ("enter the string value");
+// let ch = prompt ("enter the charactor");
+// let count =1;
+
+for (let index in val){
+    if(val[index].toLowerCase()=== ch.toLowerCase()){
+        console.log("ocerance position = ",index)
+        count++;
+    }
+}
+console.log("count = ", count);
+
+// question num 3 counting the words
+
+let vol = "hello who are you";
+console.log(vol.split(" ").length);
 
 
 
