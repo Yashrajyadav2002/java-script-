7f4d11205a4c206ba4ad46ae00182044f81b0a2c
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-20 13:32:29 +0530

    funcation

---
+++ 30 APRIL/20 may funcation.html/20may funcation .js
@@ -0,0 +1,22 @@
+// life (imedetly invoked funcation)
+
+(function (){
+    var x = "hello";
+    console.log(x);
+})();
+
+(function (){
+
+    let name = prompt ("enter your name");
+    let age = prompt ("enter your age");
+    let city = prompt ("enter your city");
+    
+console.log(name);
+console.log(age);
+console.log(city);
+    
+})();
+
+//when a web page is loaded in browser it converts the html document into dom (document object model)
+//DOM is a tree of element 
+// in witch all the elements trated like objects 
