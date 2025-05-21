1bfd7148cc2ebf18ea4d8966a77e0ecb5f4c3834
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-21 13:31:34 +0530

    dom

---
+++ 30 APRIL/20 may funcation.html/21 may DOM  odel.html/DOM model.js
@@ -0,0 +1,40 @@
+// these are the methods of DOM 
+
+// element get by (ID)
+
+// let h1 = document.getElementById("demo");
+
+
+// element get by (TAG NAME);
+
+// let h1 = document.getElementsByTagName("h1");
+
+// console.log(h1[0]);
+
+// let h1 = document.getElementsByClassName("head");
+// console.log(h1[0]);
+
+// let h1 = document.querySelector(".head");
+// // console.log(h1);
+
+// let h1 = document.getElementById("demo");
+// h1.innerText = "hello world";
+
+// // let name = prompt("enter name");
+// let h1 = document.getElementById("demo2");
+// // h1.innerText=name
+// h1.innerHTML = `<p> HI who are you bro</p>`;
+
+// let h1 = document.getElementById("demo2");
+// h1.innerHTML = `<table border>
+// <tr>
+//     <th>name</th>
+//     <th>age</th>
+// </tr>
+// <tr>
+// <td>yash</td>
+// <td>23</td>
+// </tr> </table>`;
+
+let img = document.getElementById("my");
+img.src = "bugati.jpg";
