b3254873e3eed71e70ecd96659f581dc989d56e3
yashrajyadav2002 <yyash9631@gmail.com>
2025-05-25 13:32:58 +0530

    mathmethodpractice

---
+++ 05 may 2025/math.js
@@ -0,0 +1,179 @@
+//    MATH TYPE OBJECTS 
+
+// floor - returns the lower value
+
+let a= Math.floor(5.780);
+console.log(a);
+
+//ceil - returns the upper value 
+
+let b = Math.ceil(3.44);
+console.log(b);
+
+//trunc - returns the integer value . it avoid the point values
+
+let c = Math.trunc(2546.365);
+console.log(c);
+
+//MIN - returns the minium value
+
+let e = Math.min(35,19,29,59,99,89,78);
+console.log(e);
+
+//MAX - returns the maximum value 
+
+let f = Math.max(56,58,77,99,35,36);
+console.log(f);
+
+//POW - returns the power value
+
+let g = Math.pow(3,3);
+console.log(g);
+
+// RANDOM = returns the random value
+
+let h = Math.random()+2;
+console.log(h);
+
+// ROUND - it returns the roundfigure value of integer
+let i = Math.round(67.54);
+console.log(i);
+
+
+
+
+let x = Math.random() * 51 +1;
+let y = Math.random() * 101 + 1;
+
+ let sum = x+y;
+ console.log(sum);
+
+ let div = x/y;
+ console.log(div);
+ 
+ let multi = x*y;
+ console.log(multi);
+
+ let min = x-y;
+ console.log(min);
+
+//      DATE TYPE OBJECTS 
+
+// by defult javascript will use the browser time line and display the date in string
+
+let d = new Date();
+
+console.log(d);
+
+// there are four ways to create object in date object
+
+let D =new Date(2025,4,3,25,36,10);
+console.log(D);
+
+let z=Date(468541684);
+console.log(z);
+
+// methods of date
+
+const year = D.getFullYear();
+console.log(year);
+
+
+const month = D.getMonth();
+console.log(month);
+
+
+const day = D.getDay("2");
+console.log(day);
+
+
+const date = D.getDate();
+console.log(date);
+
+const hour = D.getHours();
+console.log(hour);
+
+const mini = D.getMinutes();
+console.log(mini);
+
+const sec = D.getSeconds();
+console.log(sec);
+
+
+// its a practice time for math methods
+// question no 0.1
+let aa = Math.floor(4.9);
+console.log(aa); // 4 ;
+
+//question no 0.2
+let bb = Math.round(9.656);
+console.log(bb);// 10; // upword
+
+// question no0.3
+let cc = Math.ceil(99.40);
+console.log(cc); // 100;
+
+// question no 0.4
+let dd = Math.min(7,2,10,0);
+console.log(dd); // 0;
+
+// question no 0.5
+let ee = Math.max(7,2,10,0);
+console.log(ee); // 10;
+
+//question no 0.6
+let ff =Math.random()*101 +1;
+console.log(ff); // done ;
+
+// quection no 0.7
+let gg = Math.trunc(-4.9);
+console.log(gg); // -4 ;
+
+// question no 0.8
+let hh = Math.pow(5,3);
+console.log(hh); // 125 ;
+
+// you have an arry of prices finn the minimum and maximum values ?
+let prices = [19.99,9.5,12.75,25.3];
+let max = Math.max(...prices);
+let min1 = Math.min(...prices);
+
+console.log(max);
+console.log(min1);
+
+// write a funcation that returns random integer any to given number ?
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min + 1)) + min ;
+}
+console.log(getRandom(10,20));
+
+// squire root ?
+
+let jj = Math.sqrt(81);
+console.log(jj); // 9
+
+// write a funcation that samulates a 6- sided dice roll using math . random method ?
+
+function randomDice(){
+    let dise =Math.floor(Math.random()*6)+1;
+    console.log(dise);
+}
+randomDice();
+
+// ROund the number 9.656 to :
+// a . the nearest whole number 
+// b . upword
+// c. downword
+
+console.log(Math.round(9.656));
+console.log(Math.ceil(9.656));
+console.log(Math.floor(9.656));
+
+// 
+
+
+
+
+
+
+
