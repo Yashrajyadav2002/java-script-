9f564b79cfcb657568818c9221a2077ba52d0f60
yashrajyadav2002 <yyash9631@gmail.com>
2025-06-01 14:05:03 +0530

    practice

--- 31 may test practice/script.js
+++ 31 may test practice/script.js
@@ -97,33 +97,33 @@ if(mark>=90){
 
 // write a program it show the day by the number ?
 
-let daynum = 2;
- switch (daynum){
-    case 1: 
-    console.log("monday");
-    break;
-    case 2: 
-    console.log("tuesday");
-    break;
-    case 3: 
-    console.log("wednesday");
-    break;
-    case 4: 
-    console.log("thursday");
-    break;
-    case 5: 
-    console.log("friday");
-    break;
-    case 6: 
-    console.log("saturday");
-    break;
-    case 7: 
-    console.log("sunday");
-    break;
+// let daynum = 2;
+//  switch (daynum){
+//     case 1: 
+//     console.log("monday");
+//     break;
+//     case 2: 
+//     console.log("tuesday");
+//     break;
+//     case 3: 
+//     console.log("wednesday");
+//     break;
+//     case 4: 
+//     console.log("thursday");
+//     break;
+//     case 5: 
+//     console.log("friday");
+//     break;
+//     case 6: 
+//     console.log("saturday");
+//     break;
+//     case 7: 
+//     console.log("sunday");
+//     break;
 
-    default:
-        console.log("invalid day number");
- }
+//     default:
+//         console.log("invalid day number");
+//  }
 
  // write a program to chek the eligblelity to driving ?
 
@@ -206,6 +206,225 @@ for(let i=1;i<10 +1;i++){
     for(let now in data){
         console.log(now,"=",data[now]);
     }
+
+
+    //!!!!!!!!!!!!!! Funcation !!!!!!!!!!!!!!!!!!
+    //funcation decleration
+    function greet (){
+      console.log("hello yash");
+    }
+    greet();
+
+    // funcation with parameter
+
+    function greetuser(name){
+      console.log("Hello, " + name + "!");
+    }
+    greetuser("yash");
+
+
+    function userji (name){
+      console.log("hello, " + name + "!");
+    };
+    userji("betu");
+
+    // funcation with return value 
+
+    function add (a,b){
+      return a+b;
+    }
+    let final = add(5,3);
+    console.log(final); //8
+
+    function area(a,b){
+      return a*b;
+    }
+    let totalarea=area(5,4);
+    console.log(totalarea);
+
+//  function Expression 
+    const multply = function(x,y){
+      return x * y;
+    };
+    console.log(multply(6,7)); // 42
+
+    const divide = function(x,y){
+      return x / y ;
+    };
+    console.log(divide(6,2));
+
+    // Arrow funcation 
+    const subtract = (a,b) => a-b;
+    console.log(subtract(5,10));
+
+    function getmax(a,b){
+      return a>b ? a : b;
+    }
+    console.log(getmax(16,15));
+
+    // chek the num is even or odd ?
+
+    function chekevenodd(num){
+      if(num % 2 ===0){
+         console.log("Even");
+      }else{
+         console.log("odd");
+      }
+    }
+    chekevenodd(24);
+
+    // funcation for power number ?
+    function powernum (base,exponent){
+      return base**exponent;
+    }
+    let next = powernum(2,3);
+    console.log(next);
+
+    // write a funcation celsious to fahrenhite ?
+
+    function celcious (celcious){
+      return F = (celcious * 9/5) +32;
+    }
+    let last =celcious(0);
+    console.log(last); // 32 ;
+
+    //chek the num is prime or not ?
+
+    function isprime (num){
+      if (num <=1) return false;
+
+      for (let i = 2; i<=Math.sqrt(num); i++){
+         if (num % i ===0){
+            return false;
+         }
+      }
+      return true;
+    }
+    console.log(isprime(7));
+    console.log(isprime(10));
+
+    // caluclator project
+
+    function add  (a,b){
+      return a+b;
+    };
+    function subtracte(a,b){
+      return a - b ;
+    }
+    function multplyy(a,b){
+      return a * b ;
+    }
+    function division (a,b){
+      if (b === 0){
+         return "cant divide by zero"
+      }
+      return a / b ;
+    }
+
+    function calculator (a,b ,operator){
+      switch (operator){
+         case '+':
+            return add (a,b);
+            case'-':
+            return subtract (a,b);
+            case '*':
+            return multplyy (a,b);
+            case '/':
+            return division (a,b);
+            default:
+               return "invalid operator !";
+            }
+      }
+      console.log(calculator(10,5, "+"));
+      console.log(calculator(10,5, "-"));
+      console.log(calculator(10,5, "*"));
+      console.log(calculator(10,5, "/"));
+    
+
+
+      // lest talk about array concepts 
+      // what is array  and array accessing and properties
+
+      let fruits = ["apple","orange","banana"];
+      console.log(fruits[1]);
+      console.log(fruits);
+      console.log(fruits.length);
+
+      // now we talking about array methods 
+
+      for (let i = 0; i<fruits.length;i++){
+         console.log(fruits[i])
+      }
+      fruits.forEach(function(fruits){
+         console.log(fruits)
+      });
+
+      let arr =[10,20,30,40,50];
+      for(let i =0; i <arr; i++){
+      
+      }
+      console.log(arr);
+      
+      // calculate the total numebr of sum ?
+      let number =[5,10,15,20];
+      let sum =0;
+      for(let i=0;i<number.length; i++){
+         sum= sum+number[i];
+      }
+      console.log("sum is",sum);
+
+      // find the samlest number using a loop ?
+
+      let numss =[23,65,87,45,900];
+      let min = numss[0];
+      for(let i =1 ;i >numss.length; i++){
+         if(numss [i] >  min) {
+            min = numss[i];
+         }
+      }
+      console.log("samllest num is",min);
+
+      let mine =Math.min(...numss);
+      console.log("the num is",mine);
+
+      // find the  largest number using a ;loop ?
+      let numr =[23,65,87,45,900];
+      let max = numr[0];
+      for(let i =1 ;i <numr.length; i++){
+         if(numr [i] > max) {
+            max = numr[i];
+         }
+      }
+      console.log("largest num is",max);
+
+      let maxi = Math.max(...numss);
+      console.log("the num is",maxi);
+
+
+      // lets count how many numbers are even ?
+
+      let datas =[3,6,9,12,15,18];
+      let count = 0;
+
+      for (let i =0 ; i<datas.length; i++){
+         if (datas [i] % 2 ===0) {
+            count++;
+         }
+      };
+      console.log("Total even number are",count);
+
+
+  //  With the help of for each and funcation method ?
+
+  let count1 =0;
+  datas.forEach(num =>{
+   if (num%2===0){
+      count1++;
+   }
+  })
+  console.log("this is the value",count1)
+
+  // reverse the number using loop (not use the . reverse method) ?
 
 
 
