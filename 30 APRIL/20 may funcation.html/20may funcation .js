3a30743eb941c91e73866a8d2989d081acd12fa4
yashrajyadav2002 <yyash9631@gmail.com>
2025-06-04 23:57:19 +0530

    funcation

--- 30 APRIL/20 may funcation.html/20may funcation .js
+++ 30 APRIL/20 may funcation.html/20may funcation .js
@@ -20,3 +20,28 @@ console.log(city);
 //when a web page is loaded in browser it converts the html document into dom (document object model)
 //DOM is a tree of element 
 // in witch all the elements trated like objects 
+
+// function start (num){
+//     if (num%2===0 || num%400===0){
+//         console.log("right")
+//     } 
+//     else{
+//         console.log("Wrong")
+//     }
+// }
+// start(2);
+
+// when we pass the c value its a optional we can change it to give it a real value
+function system (a,b , c = 5){
+    return a+b+c ;
+}
+result1 = system(5,3);
+result2 = system(9,3);
+result3 = system(10,3 ,2);
+result4 = system(11,3);
+
+console.log("this is the sum of num is",result1);
+console.log("this is the sum of num is",result2);
+console.log("this is the sum of num is",result3);
+console.log("this is the sum of num is",result4);
+
---
+++ DOM sidhart sir 04 june/script.js
@@ -0,0 +1,6 @@
+// always javascript print the array 
+let ans = confirm 
+
+let =1234;
+console.log( Number(numbers.toString().split("").reverse().join("")));
+
