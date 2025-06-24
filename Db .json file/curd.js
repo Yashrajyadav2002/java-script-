eddb581ca29442c420be3466b79895ce0c1b0c42
yashrajyadav2002 <yyash9631@gmail.com>
2025-06-24 23:16:35 +0530

    newadd

--- Db .json file/curd.js
+++ Db .json file/curd.js
@@ -21,9 +21,76 @@ let FatchData=async()=>{
 
     
 )}
+let confirm=(id)=>{
+
+    Swal.fire({
+
+        title:"Are you sure",
+        text:"You wont't be able to revert this",
+        icon:"Warning",
+        shwcancelButton: true,
+        confirmButtonColor:"#3005d6",
+        cancelButtonColor:"#d33",
+        confirmbuttonText:"yes,delete it!"
+    }).than((result)=>{
+        if(result.isconfirmed){
+            Del(id)
+            Swal.fire({
+                title: "Deleted",
+                text: "Your faile has been deteted",
+                icon: "Success"
+            });
+        }
+    });
+}
 
 let del=(id)=>{
     fetch(`http://localhost:3000/carrent/${id}`,{method:"DELETE"})
 }
 
 FatchData();
+
+let ins=()=>{
+
+    let name=document.querySelector("name").value
+    let age=document.querySelector("age").value
+    let mobile=document.querySelector("mobile").value
+    let adhar=document.querySelector("agar").value
+    let checkin=document.querySelector("checkin").value
+    let checkout=document.querySelector("checkout").value
+    let city=document.querySelector("city").value
+    let people=document.querySelector("people").value
+
+
+    let url='http://localhost:3000/carrent'
+
+    fetch(url ,{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+
+        body: JSON.stringify(
+            {
+               name:name,
+               age:age,
+               mobile:mobile,
+               adhar:adhar,
+               checkin:checkin,
+               checkout:checkout,
+               city:city,
+               people:people,
+
+            }
+
+        )
+    })
+
+
+    location.href="index.html"
+    return false
+
+}
+
+
+
