b0f7a04eac01708c1ef4b9a96959ddc65dd3cf7d
yashrajyadav2002 <yyash9631@gmail.com>
2025-07-12 13:54:35 +0530

    paradiam

---
+++ Paradigam/para.js
@@ -0,0 +1,21 @@
+// it is a way or style of programing
+
+//0.1 imprative paradigm
+ // 0.0 productal 2. stractured
+
+ //0.2 Declarative paradigm
+   //1. Funcational 2. reactive 
+
+   // 0.3 object-oreanted programing
+
+   // Event diven
+
+   // Programing paradigm - it is the way of programing .
+
+   let arr = [1,2,3,4,5,6];
+
+//    for(let i = 0; i <arr.length; i++){
+//     console.log(arr[i]);
+//    };
+
+   arr.map(Element=>console.log(Element));
