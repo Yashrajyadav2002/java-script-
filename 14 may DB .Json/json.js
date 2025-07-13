caba23fb230b9bb3467ffd7eff7be7d2dc007b38
yashrajyadav2002 <yyash9631@gmail.com>
2025-07-13 14:14:38 +0530

    localstorage

--- 14 may DB .Json/json.js
+++ 14 may DB .Json/json.js
@@ -129,3 +129,14 @@ console.log(seq);
 
 
 
+// local storage 
+
+let array = {name:"yash",age:23 ,city:"bhopal"};
+
+let showdata=()=>{
+    localStorage.setItem("see data",JSON.stringify(array))
+}
+
+let get =()=>{
+    localStorage.setItem(JSON.parse(localStorage.getItem("see data")))
+};
