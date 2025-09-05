0947b6cfe16a07dee599370b92973ef83d18c3e6
yashrajyadav2002 <yyash9631@gmail.com>
2025-09-05 14:15:20 +0530

    05

--- 05 may 2025/newdate.js
+++ 05 may 2025/newdate.js
@@ -1,7 +1,7 @@
 // its a practice time for date object method in java script
 
 // 0.1 how do you create a new date object with the curent date and time ?
-let a = new Date();
+let a1 = new Date();
 console.log(a);
 
 // // question no 0.2
@@ -22,7 +22,7 @@ console.log(a);
  // display date in 25-may-2025 format ?
 
  let value = new Date("2025-05-25");
- let day1 = value.getDate();
+ let day3 = value.getDate();
  let monthNames = ["jan","feb","mar","apr","may","june","jul","aug","sep","oct","nov","dec"];
 
  let month = monthNames[value.getMonth()];
@@ -30,4 +30,39 @@ console.log(a);
  let formattedDate = `${day1}-${month}-${year}`;
 
  console.log(formattedDate);
+
+
+ // its a practice time for date object method in java script
+
+// 0.1 how do you create a new date object with the curent date and time ?
+let a = new Date();
+console.log(a);
+
+// // question no 0.2
+// let d = new Date("2022-05-25");
+// console.log(d.getfullyear());
+
+// how to get current day of  a week ?
+ let today1 = new Date();
+ let day1 = today.getDay();
+ console.log(day); // i have dout
+
+ // how to get the hours from a date objest ?
+
+ let time1 = new Date();
+ let hours1 = time.getHours();
+ console.log(hours);
+
+ // display date in 25-may-2025 format ?
+
+ let value1 = new Date("2025-05-25");
+ let day2 = value.getDate();
+ let monthNames1 = ["jan","feb","mar","apr","may","june","jul","aug","sep","oct","nov","dec"];
+
+ let month1 = monthNames[value.getMonth()];
+ let year1 = value.gerFullYear();
+ let formattedDate1 = `${day1}-${month}-${year}`;
+
+ console.log(formattedDate);
+
 
