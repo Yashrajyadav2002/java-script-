d61b5abbdad357c45a724703ef9dd1a6036bc949
yashrajyadav2002 <yyash9631@gmail.com>
2025-09-18 23:30:39 +0530

    18

--- 13 may/13new.js
+++ 13 may/13new.js
@@ -26,5 +26,39 @@ console.log(x);
  }
 
 
+//  !!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+// question number 3
+let arr2 = [1,2,3,4,5,6,7,8,9,10];
+arr.reverse();
+console.log(arr2);
+// question number 4
+let d2 =new Date();
+
+console.log(d2);
+// question number 5
+
+let x2 = ((Math.random()*15)+1);
+console.log(x2);
+
+//question number 2
+
+ let sum2 = [1+2+3+4+5+6+7+8+9+10];
+ sum.add;
+ console.log(sum2);
+
+ // question no 1
+ let ar2 =[];
+ for (let i = 0; i<10; i++){
+    ar[i] = pareseInt(prompt())
+ 
+ console.log(ar2);
+ }
+
+
+
+
+
+
 
 
