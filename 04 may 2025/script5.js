11b6134ecf2c29ec680f99b8ddc2eb8f43dc0862
yashrajyadav2002 <yyash9631@gmail.com>
2025-09-29 23:51:32 +0530

    29

--- 04 may 2025/script5.js
+++ 04 may 2025/script5.js
@@ -13,3 +13,20 @@ console.log(double);
 let name =`my name is `;
 let backtik = `Hello , ${name}! yashraj `;
 console.log(backtik);
+
+
+// single coat string ?
+
+let single1 = " HELLO BROTHER ";
+console.log(single1);
+
+//double coat string?
+
+let double1 = 'who are you';
+console.log(double1);
+
+//backticks (template literals)?
+
+let name1 =`my name is `;
+let backtik1 = `Hello , ${name1}! yashraj `;
+console.log(backtik1);
