cf09d435f5711cd9dfa1cd51145489440aae9c18
yashrajyadav2002 <yyash9631@gmail.com>
2025-10-15 18:50:42 +0530

    practice

--- 05 may 2025/number.js
+++ 05 may 2025/number.js
@@ -50,3 +50,57 @@ console.log(str.toString().length);
 
 
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//  these are literal methods in number declrition 
+let num4 = 12345;
+let num6 = "12345";
+let num8 = '12345';
+console.log(num4 , typeof num4);
+console.log(num6 , typeof num6);
+console.log(num8 , typeof num8);
+
+//constructor method in numbers 
+// it allways a number in any condition
+let num9 = new Number (123456);
+console.log(num9);
+ // we chek the it is integer or not
+
+ let val3 = 123.456;
+ console.log(Number.isInteger(val3));
+
+ // parseint -  we can change the value  float into  integer?
+ let val4 = 125.56;
+ console.log(Number.parseInt(val4));
+ 
+ // parsefloat - we can change the value integer into float
+ let val5 = '125.384px';
+ console.log(Number.parseFloat(val5));
+
+ // isfinite - we can chek the infinte vale ?
+
+ let infi1 = 1/0;
+ console.log(Number.isFinite(infi1));
+
+ // number instance method // used on number premative?
+   
+// (toFixed) - it take the value after the point ?
+
+let fix1 = 123456;
+console.log(fix1.toFixed(3));
+
+// toexponstional - 
+
+// precision - it return the pre size value (round figer value)
+
+let pree1 = 123.46;
+console.log(pree1.toPrecision(3));
+
+
+// tostring -  it convert the number into string ?
+
+let str1 = 1234;
+console.log(str1.toString().length);
+
+
+
