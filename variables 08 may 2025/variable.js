625969554866afb97426ba545060eb5e294a7a62
yashrajyadav2002 <yyash9631@gmail.com>
2025-10-27 23:29:57 +0530

    repct

--- variables 08 may 2025/variable.js
+++ variables 08 may 2025/variable.js
@@ -61,3 +61,55 @@ c3 = 30,
 d4 = 40 ;
 console.log(a,b,c,d);
 
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// console is print the output
+console.log("Yashraj");
+
+let name2 ; //Declaration
+ name2 = "Yashraj Yadav"; //iniclazition
+console.log(name2); // print the value by the console
+
+let village4 = "Bhopal";
+console.log(village4);
+
+let a3 = 10,
+b3 = 20,
+c4 = 30,
+d3 = 40 ;
+console.log(a3,b3,c4,d3);
+
+// const value is not change in feature
+
+const pi3 = 20.1;
+console.log(pi);
+
+
+let village5 = "Bhopal";
+console.log(villag5);
+
+let a5 = 10,
+b5 = 20,
+c5 = 30,
+d5 = 40 ;
+console.log(a5,b5,c5,d5);
+
+
+// console is print the output
+console.log("Yashraj");
+
+let name6 ; //Declaration
+ name2 = "Yashraj Yadav"; //iniclazition
+console.log(name6); // print the value by the console
+
+let village6 = "Bhopal";
+console.log(village6);
+
+let a8 = 10,
+b8 = 20,
+c8 = 30,
+d8 = 40 ;
+console.log(a4,b4,c1,d1);
+
+
