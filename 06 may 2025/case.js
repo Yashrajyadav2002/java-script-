c908f17b35784ca3cdc301047a58698f9c672452
yashrajyadav2002 <yyash9631@gmail.com>
2026-01-23 22:23:15 +0530

    23january

--- 06 may 2025/case.js
+++ 06 may 2025/case.js
@@ -58,3 +58,36 @@ default:console.log("wrong input");
 
 
 
+// repeat
+
+let d3 = new Date();
+const day3 = d.getDay();
+console.log(day3);
+switch(day){
+
+case 0:console.log("sunday")
+break;
+
+case 1:console.log("monday")
+break;
+
+case 2:console.log("tuesday")
+break;
+
+case 3:console.log("wednesday")
+break;
+
+case 4:console.log("thursday")
+break;
+
+case 5:console.log("friday")
+break;
+
+case 6:console.log("saturday")
+break;
+
+default:console.log("wrong input");
+}
+
+
+
