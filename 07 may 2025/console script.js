0d3940ee389a545213374797274514181f527306
yashrajyadav2002 <yyash9631@gmail.com>
2026-01-25 23:52:19 +0530

    re

--- 07 may 2025/console script.js
+++ 07 may 2025/console script.js
@@ -1,3 +1,7 @@
 console.log("hello my name is yashraj");
 console.error("this is an error fix it");
 console.warn("this is warrning solve it");
+
+console.log("hello this is new web page for my web");
+console.error("this is an err please try again letter");
+console.warn("this is warrning take care about it");
