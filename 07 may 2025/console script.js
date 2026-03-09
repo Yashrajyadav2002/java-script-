8c57e6d65544482cbecc8ac9a4e792aa80a2b1cb
yashrajyadav2002 <yyash9631@gmail.com>
2026-03-09 21:49:18 +0530

    24

--- 07 may 2025/console script.js
+++ 07 may 2025/console script.js
@@ -5,3 +5,11 @@ console.warn("this is warrning solve it");
 console.log("hello this is new web page for my web");
 console.error("this is an err please try again letter");
 console.warn("this is warrning take care about it");
+
+console.log("hello my name is yashraj");
+console.error("this is an error fix it");
+console.warn("this is warrning solve it");
+
+console.log("hello this is new web page for my web");
+console.error("this is an err please try again letter");
+console.warn("this is warrning take care about it");
